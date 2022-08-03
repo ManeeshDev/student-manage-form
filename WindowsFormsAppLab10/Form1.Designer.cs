b9ba421fca894c88931d6b687109ab2cf80ebee5
ManeeshDev <manishprashangamage23@gmail.com>
2022-08-03 21:38:40 +0530

    Add files

---
+++ WindowsFormsAppLab10/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace WindowsFormsAppLab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtGpa = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtStName = new System.Windows.Forms.TextBox();
+            this.txtStID = new System.Windows.Forms.TextBox();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.studentTableView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.studentTableView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl2);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Controls.Add(this.btnSubmit);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Controls.Add(this.txtGpa);
+            this.panel1.Controls.Add(this.txtAge);
+            this.panel1.Controls.Add(this.txtStName);
+            this.panel1.Controls.Add(this.txtStID);
+            this.panel1.Controls.Add(this.lbl5);
+            this.panel1.Controls.Add(this.lbl4);
+            this.panel1.Controls.Add(this.lbl3);
+            this.panel1.Location = new System.Drawing.Point(41, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(542, 252);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl2.Location = new System.Drawing.Point(66, 57);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(87, 13);
+            this.lbl2.TabIndex = 0;
+            this.lbl2.Text = "Student Name";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl1.Location = new System.Drawing.Point(66, 17);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(68, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Student ID";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Gray;
+            this.btnSubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnSubmit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnSubmit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSubmit.Location = new System.Drawing.Point(398, 215);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtAddress.Location = new System.Drawing.Point(221, 173);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(253, 21);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // txtGpa
+            // 
+            this.txtGpa.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtGpa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGpa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGpa.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtGpa.Location = new System.Drawing.Point(221, 133);
+            this.txtGpa.Name = "txtGpa";
+            this.txtGpa.Size = new System.Drawing.Size(253, 21);
+            this.txtGpa.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtAge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtAge.Location = new System.Drawing.Point(221, 93);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(253, 21);
+            this.txtAge.TabIndex = 3;
+            // 
+            // txtStName
+            // 
+            this.txtStName.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtStName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtStName.Location = new System.Drawing.Point(221, 53);
+            this.txtStName.Name = "txtStName";
+            this.txtStName.Size = new System.Drawing.Size(253, 21);
+            this.txtStName.TabIndex = 2;
+            // 
+            // txtStID
+            // 
+            this.txtStID.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtStID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStID.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtStID.Location = new System.Drawing.Point(221, 13);
+            this.txtStID.Name = "txtStID";
+            this.txtStID.Size = new System.Drawing.Size(253, 21);
+            this.txtStID.TabIndex = 1;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl5.Location = new System.Drawing.Point(66, 177);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(52, 13);
+            this.lbl5.TabIndex = 0;
+            this.lbl5.Text = "Address";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl4.Location = new System.Drawing.Point(66, 137);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(32, 13);
+            this.lbl4.TabIndex = 0;
+            this.lbl4.Text = "GPA";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl3.Location = new System.Drawing.Point(66, 97);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(29, 13);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.Text = "Age";
+            // 
+            // studentTableView
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.GrayText;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Info;
+            this.studentTableView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.studentTableView.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.studentTableView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.studentTableView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.studentTableView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.studentTableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.studentTableView.DefaultCellStyle = dataGridViewCellStyle3;
+            this.studentTableView.EnableHeadersVisualStyles = false;
+            this.studentTableView.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.studentTableView.Location = new System.Drawing.Point(41, 360);
+            this.studentTableView.Name = "studentTableView";
+            this.studentTableView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.studentTableView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.studentTableView.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.studentTableView.Size = new System.Drawing.Size(542, 236);
+            this.studentTableView.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(93, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "STUDENT MANAGEMENT SYSTEM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(69)))));
+            this.ClientSize = new System.Drawing.Size(624, 631);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.studentTableView);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(640, 670);
+            this.MinimumSize = new System.Drawing.Size(640, 670);
+            this.Name = "Form1";
+            this.Opacity = 0.96D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.studentTableView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtStID;
+        private System.Windows.Forms.TextBox txtStName;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtGpa;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.DataGridView studentTableView;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsAppLab10/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsAppLab10
+{
+    public partial class Form1 : Form
+    {
+
+        String conString;
+        SqlConnection con;
+
+        int studentID;
+        String studentName;
+        int age;
+        double gpa;
+        String address;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string getConnectionString()
+        {
+            string conString;
+
+            conString = @"Data Source=MANEESH-ASUS-TU\SQLEXPRESS;Initial Catalog=Student;Integrated Security=True";
+
+
+            return conString;
+        }
+
+        public void loadData()
+        {
+            conString = getConnectionString();
+            con = new SqlConnection(conString);
+            con.Open();
+            String sql = "SELECT * FROM dbo.Student";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, con);
+
+            SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(dataAdapter);
+
+            DataTable table = new DataTable();
+
+            dataAdapter.Fill(table);
+
+            studentTableView.DataSource = table;
+
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conString = getConnectionString();
+
+                con = new SqlConnection(conString);
+                con.Open();
+
+                if ((txtStName.Text == "" || txtStName.Text == null) && (txtStID.Text == "" || txtStID.Text == null) &&
+                    (txtAge.Text == "" || txtAge.Text == null) && (txtGpa.Text == "" || txtGpa.Text == null) &&
+                    (txtAddress.Text == "" || txtAddress.Text == null))
+                {
+                    MessageBox.Show("Please fill all fields!!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                studentName = txtStName.Text;
+                studentID = Int32.Parse(txtStID.Text);
+                age = Int32.Parse(txtAge.Text);
+                gpa = Convert.ToDouble(txtGpa.Text);
+                address = txtAddress.Text;
+
+                string insertQuery = "INSERT INTO dbo.Student VALUES (@studentID, @studentName, @age, @gpa, @address)";
+
+                SqlCommand insertCmd = new SqlCommand(insertQuery, con);
+                insertCmd.Parameters.AddWithValue("@studentID", studentID);
+                insertCmd.Parameters.AddWithValue("@studentName", studentName);
+                insertCmd.Parameters.AddWithValue("@age", age);
+                insertCmd.Parameters.AddWithValue("@gpa", gpa);
+                insertCmd.Parameters.AddWithValue("@address", address);
+
+                insertCmd.ExecuteNonQuery();
+
+                con.Close();
+
+                txtStName.Text = "";
+                txtStID.Text = "";
+                txtAge.Text = "";
+                txtGpa.Text = "";
+                txtAddress.Text = "";
+
+                MessageBox.Show("Successfully Saved..", " Student Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                loadData();
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+
+    }
+}
